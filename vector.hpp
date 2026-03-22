be7b068147a49967bf5385df96f2f9182d706ae4
kstudilova <kstudilova@gmail.com>
2026-03-22 23:33:59 +0300

    add getSize, getCapacity, pushBack, popBack

--- vector.hpp
+++ vector.hpp
@@ -17,17 +17,17 @@ namespace topit {
       Vector& operator=(Vector&&);
 
       bool isEmpty() const noexcept; //дз
-      bool getSize() const noexcept; //дз
-      bool getCapacity() const noexcept; //дз
+      size_t getSize() const noexcept; //дз
+      size_t getCapacity() const noexcept; //дз
 
-      void popBack(); //дз
       void pushBack(const T& value); //дз
+      void popBack(); //дз
       void insert(size_t i, const T& v);
       void erase(size_t i);
 
     private:
-      T* data;
-      size_t size, cap;
+      T* data_;
+      size_t size_, cap_;
   };
 }
 
@@ -45,12 +45,42 @@ topit::Vector< T >::~Vector() {
 
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept {
-  return !size_;
+  return size_ == 0;
+}
+
+template< class T >
+size_t topit::Vector< T >::getSize() const noexcept {
+  return size_;
+}
+
+template< class T >
+size_t topit::Vector< T >::getCapacity() const noexcept {
+  return cap_;
 }
 
 template< class T >
 void topit::Vector< T >::pushBack(const T& value) {
+  if (size_ == cap_) {
+    size_t newCap = (cap_ == 0) ? 1 : cap_ * 2;
+    T* newData = new T[newCap];
 
+    for (size_t i = 0; i < size; ++i) {
+      newData[i] = data_[i];
+    }
+
+    delete data_[];
+    data_ = newData;
+    cap_ = newCap;
+  }
+
+  data_[size_] = value;
+  ++size_;
+}
+
+template< class T >
+void topit::Vector< T >::popBack() {
+  data[size_].~T();
+  --size_;
 }
 
 #endif
