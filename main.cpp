afcb759e468a966d463791e10d21008b58daec64
kstudilova <kstudilova@gmail.com>
2026-03-22 22:48:17 +0300

    add min test framework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <iomanip>
 #include "vector.hpp"
 
-int main() {
+bool testEmptyVector() {
+    return false;
+}
 
+int main() {
+    using test_t = std::pair< const char *, bool(*)() >;
+    test_t tests[] = {
+        { "Empty vector", testEmptyVector }
+    };
+
+    const size_t count = sizeof(tests) / sizeof(test_t);
+    for (size_t i = 0; i < count; ++i) {
+        std::cout << tests[i].first << ": " << tests[i].second() << "\n";
+    }
+    return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -3,10 +3,31 @@
 
 #include <cstddef>
 
+namespace topit {
+  
+  template< class T >
+  struct Vector {
+    
+    public:
+      Vector();
+      ~Vector();
+
+    private:
+      T* data;
+      size_t size, cap;
+  };
+}
+
 template< class T >
-struct Vector {
-  T* data;
-  size_t size, cap;
-};
+topit::Vector< T >::Vector() :
+  data_(nullptr),
+  size_(0),
+  cap_(0),
+{}
+
+template< class T >
+topit::Vector< T >::~Vector() {
+  delete[] data_;
+}
 
 #endif
