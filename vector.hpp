0ec5857927c91dec273274c5b1ec65fd56b9e1f5
kstudilova <kstudilova@gmail.com>
2026-03-24 10:08:03 +0300

    out of bout at pass

--- vector.hpp
+++ vector.hpp
@@ -89,11 +89,28 @@ void topit::Vector< T >::popBack() {
 }
 
 template< class T >
+T& topit::Vector< T >::operator[](size_t id) noexcept {
+  return data_[id];
+}
+
+template< class T >
+const T& topit::Vector< T >::operator[](size_t id) const noexcept {
+  return data_[id];
+}
+
+
+template< class T >
+T& topit::Vector< T >::at(size_t id) {
+  const Vector< T > * cthis = this;
+  return const_cast< T& >(cthis->at(id));
+}
+
+template< class T >
 const T& topit::Vector< T >::at(size_t id) const {
   if (id = getSize()) {
     return data_[id];
   }
-  throw std::range_error("bad id");
+  throw std::out_of_range("bad id");
 }
 
 #endif
