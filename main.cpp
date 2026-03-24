4d58b3d8ff00f8fa5b15642c8858219a8fc9b365
kstudilova <kstudilova@gmail.com>
2026-03-24 10:57:15 +0300

    testCopyConstructorFotNonEmpty have to be failed but it successed

--- main.cpp
+++ main.cpp
@@ -125,7 +125,12 @@ bool testCopyConstructorForNonEmpty() {
     topit::Vector< int > v;
     v.pushBack(1);
     topit::Vector< int > yav = v;
-    return v == yav;
+    try {
+        return yav.getSize() == v.getSize() && yav.at(0) == v.at(0);
+    }
+    catch (...) {
+        return false;
+    }
 }
 
 //тесты для operator==
